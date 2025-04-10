a35d4de0ca88a863863bde96d0f03fda2e5f8911
Ali Baqbani <alibaqbani@gmail.com>
2025-04-10 14:23:18 +0200

    Setup project

---
+++ src/SwiftArc/SwiftArcApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftArcApp.swift
+//  SwiftArc
+//
+//  Created by Ali Baqbani on 4/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftArcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ src/SwiftArcTests/SwiftArcTests.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftArcTests.swift
+//  SwiftArcTests
+//
+//  Created by Ali Baqbani on 4/10/25.
+//
+
+import Testing
+
+struct SwiftArcTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
